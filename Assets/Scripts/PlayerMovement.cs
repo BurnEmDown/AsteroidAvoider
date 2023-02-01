26e41c932a4ec377f271f79a3f1f79966f4673ec
BurnEmDown <amitreuveni@gmail.com>
2023-02-02 01:48:49 +0200

    Added ship movement with touch

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,13 @@ using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float forceMagnitude;
+    [SerializeField] private float maxVelocity;
+    
     private Camera mainCamera;
+
+    private Vector3 movementDirection;
 
     private void Start()
     {
@@ -35,8 +41,23 @@ public class PlayerMovement : MonoBehaviour
         {
             Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
             Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
-            
-            Debug.Log(worldPosition);
+
+            movementDirection = transform.position - worldPosition;
+            movementDirection.z = 0f;
+            movementDirection.Normalize();
         }
+        else
+        {
+            movementDirection = Vector3.zero;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (movementDirection == Vector3.zero) return;
+        
+        rb.AddForce(movementDirection * forceMagnitude, ForceMode.Force);
+
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity);
     }
 }
